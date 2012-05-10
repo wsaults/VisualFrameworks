5b32a2931cce34cc930d39d81b46d4716c9d9665
William Saults <wsaults@email.itt-tech.edu>
2012-05-09 22:53:29 -0500

    Started adding to the js file.

--- main.js
+++ main.js
@@ -4,18 +4,37 @@ Name: William Saults
 Term: 0512
 */
 
-var storeData = function () {
-	
-};
+// Wait until the DOM is ready
+window.addEventListener("DOMContentLoaded", function(){
 
-var gatData = function () {
+	// getElementById Function
+	function $(arg){
+		var element = document.getElementById(arg);
+		return element;
+	}
 	
-};
+	// Links and Submit Click Events
+	var submit = $('submit');
+	submit.addEventListener("click", storeData);
+	
+	var clearAll = $('clearAll', clearLocalData);
+	clearAll.addEventListener("click", clearLocalData);
+	
+	var storeData = function () {
+		console.log("Storing data.");
+	};
+	
+	var gatData = function () {
+		
+	};
+	
+	var makeCats = function () {
+		
+	};
+	
+	var clearLocalData = function () {
+		
+	};
 
-var makeCats = function () {
-	
-};
 
-var storeData = function () {
-	
-};
+}); // End "DOMContentLoaded" listener
