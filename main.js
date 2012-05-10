d33c6fabfd7d4ec374bb0b7b934f3831663826de
William Saults <wsaults@email.itt-tech.edu>
2012-05-09 21:11:54 -0500

    Added the js file.

---
+++ main.js
@@ -0,0 +1,21 @@
+/* 
+Project: Deliverable 2
+Name: William Saults
+Term: 0512
+*/
+
+var storeData = function () {
+	
+};
+
+var gatData = function () {
+	
+};
+
+var makeCats = function () {
+	
+};
+
+var storeData = function () {
+	
+};
